3f06c70298592199c2326fda799f36caeeb0a010
Md_Auntor_Shak <53232134+AuntorSheikh@users.noreply.github.com>
2021-03-08 16:00:14 +0600

    Add files via upload

---
+++ PorintaBirthday/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <dos.h>
+using namespace std;
+
+int main()
+{
+	char h = '\3';
+	cout << endl << endl << endl << endl << endl << endl << "\t\t\t" << h <<"Forget the past; look forward to the future, for the best things are yet to come"<< endl << "\t\t\tYou always bring a sweet smile to my face! Happy birthday!" << endl << "\t\t\t Thank you for being that friend, and happy birthday." << h << endl << endl << endl;
+	Sleep(5000);
+	Beep(523,500);
+	system("cls");
+	cout << endl << endl << endl << "\t\t\tFrom: \t Auntor" << endl << "\t\t\tTo:\t Porinita" << endl << "\t\t";
+	Sleep(500);
+
+
+	for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+	Sleep(500);
+	Beep(523, 500);
+
+	cout << endl << endl << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t__i_i_i_i_i__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t       |" << h << "\t    " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t     __| " << h <<"3/8/2021 " << h << " |__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t    | " << h << "    " << h << "     " << h << "    " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t  __|" << h << "   HAPPY   " << h << "     " << h << "|__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "     " << h << "   BIRTHDAY    " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |" << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "  " << h << "   PORINITA "   << h << "   " << h << "   |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |";
+	for (int i = 0; i < 25; i++) {
+		cout << "_";
+	}
+	cout << "|" << endl<<endl<<endl;
+
+	cout << "\t\tHAPPY BIRTHDAY";
+	cout << endl << "\t\t\t\tPORINITA";
+	cout << endl << "\t\t\t\t\t\tTAKE";
+	cout << endl << "\t\t\t\t|||\t |||";
+	cout << endl << "\t\t\t\t\t\t\tLOVE ";
+
+
+}
